7f6f5916562a24c7cfdf31bf650bc556b00ad4ca
Rahul Rawat <rahulrawatr320@gmail.com>
2025-10-21 09:49:57 +0530

    Data types in cpp

---
+++ file_2.cpp
@@ -0,0 +1,44 @@
+// Data types in C++
+
+#include<iostream>
+using namespace std;
+int main(){
+  // integer
+  int age=20;
+
+  // float
+  float height=187.54;
+
+  // char
+  char grade='A';
+
+  // string
+  string name= "Rahul";
+
+  // bool
+  bool isPass= true;
+
+  // double
+  double speed= 12.33334342425252;
+
+  cout<<"Name: "<<name<<endl;
+  cout<<"Age: "<<age<<endl;
+  cout<<"Height: "<<height<<endl;
+  cout<<"Grade: "<<grade<<endl;
+  cout<<"Speed: "<<speed<<endl;
+  cout<<"Passed: "<<isPass<<endl;
+
+  return 0;
+
+}
+
+// Output
+
+/*
+Name: Rahul
+Age: 20
+Height: 187.54
+Grade: A
+Speed: 12.3333
+Passed: 1
+*/
